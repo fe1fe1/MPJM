13993da657b74927b9aeefbf82d9050414de1744
feifei <francogm98@hotmail.com>
2021-02-06 21:18:36 -0300

    Version estable, con cambio de assets (disparo y pj) + animaciones

---
+++ escena.cpp
@@ -0,0 +1,63 @@
+#include "escena.h"
+#include "temps_mpjm.h"
+#include <typeinfo>
+#include <iostream>
+escena::escena()
+{
+}
+
+void escena::update(float elapsed) {
+	for (auto e : entidades) {
+		e->update(elapsed);
+	}
+	
+	//Para debug----------/
+//	for (auto e:a_borrar){
+//		std::cout<<"a borrar: "<<e<<std::endl;
+//	}
+	//--------------------/
+	
+	t_mpjm::borrar(a_borrar,entidades);
+	
+	//Para debug----------/
+//	for (auto e:entidades){
+//		std::cout<<"entidades: "<<e<<std::endl;
+//	}
+	//--------------------/
+	for (auto d : entidades) {
+		if (typeid (*d) == typeid (disparo_pj)){
+			sf::Vector2f dispPos=d->darPos();
+			if(dispPos.y<0){
+				std::cout<<"DISPARO FUERA DE PANTALLA"<<std::endl;
+				disparosPj_borrar.push_back(d);
+				quitar(d);
+			}
+		}
+	}
+}
+void escena::procesar_evento(const sf::Event &e) {
+	
+}
+void escena::draw(sf::RenderWindow &w) {
+	for (auto e : entidades) {
+		e->draw(w);
+	}	
+}
+bool escena::termino (){
+	if (cerrar == true){
+		return true;
+	}else{
+		return false;
+	}
+}
+void escena::agregar(entidad *e) {
+	entidades.push_back(e);
+}
+void escena::quitar(entidad* e) {
+	a_borrar.push_back(e);
+}
+void escena::libera_memoria(){
+	for(int i=0;i<a_borrar.size();i++) { 
+		delete a_borrar[i];
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Window/VideoMode.hpp>
+#include <vector>
+#include "escena.h"
+#include "juego.h"
+#include "escena_juego.h"
+#include "escena_menu.h"
+#include "global.h"
+int main()
+{
+	escena *escena = new escena_menu();
+	juego &j = juego::crear(sf::VideoMode(640, 400), escena, "IntentoSFML");
+	j.correr();
+	return 0;
+}
+
+
